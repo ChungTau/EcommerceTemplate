7b224bbac16da3bac5c9a8982da1d56adcbf9488
Edward Chan <edward.chan@wisespotgroup.com>
2024-03-18 16:17:58 +0800

    Update Header

--- ecommerce-webapp/src/components/header/BrandTitle.tsx
+++ ecommerce-webapp/src/components/header/BrandTitle.tsx
@@ -1,7 +1,8 @@
 import { APP_TITLE } from "@/config/app";
+import { LinkWithLocale } from "./LinkWithLocale";
 
 export default function BrandTitle(){
     return (
-        <a className="mt-1 mx-2 md: flex-none font-kalam text-xl items-center justify-center font-bold dark:text-white text-zinc-700" href="/" aria-label="Brand">{APP_TITLE}</a>
+        <LinkWithLocale className="mt-1 mx-2 md: flex-none font-kalam text-xl items-center justify-center font-bold dark:text-white text-zinc-700" href="/" aria-label="Brand">{APP_TITLE}</LinkWithLocale>
     );
 };
--- ecommerce-webapp/src/components/header/LinkWithLocale.tsx
+++ ecommerce-webapp/src/components/header/LinkWithLocale.tsx
@@ -7,17 +7,18 @@ import clsx from "clsx";
 interface LinkWithLocaleProps extends ChildrenProps {
     locale?: string | null;
     href?: string | null;
+    className?: string | null; 
 }
 
 export const LinkWithLocale = React.forwardRef<HTMLAnchorElement, LinkWithLocaleProps>(
-    ({ locale, href, children }: LinkWithLocaleProps, ref) => {
+    ({ locale, href, children, className }: LinkWithLocaleProps, ref) => {
         const pathName = usePathname();
         const [basePath, subRoute] = pathName.split("/").slice(1);
 
         const resolvedLocale = locale ?? basePath ?? "en";
         const resolvedHref = href ?? (subRoute ? `/${subRoute}` : "/");
 
-        const linkClass = clsx("group inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-md font-medium hover:bg-gray-100 hover:dark:bg-gray-100/[0.1]");
+        const linkClass = className?? clsx("group inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-md font-medium hover:bg-gray-100 hover:dark:bg-gray-100/[0.1]");
 
         return (
             <Link href={`/${resolvedLocale}${resolvedHref}`} passHref ref={ref} className={href ? linkClass : undefined}>
